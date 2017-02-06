5ec7a8a0c3085dbcd8d9283b7f89096bdb86191e
Paweł Dużalski <oddehh@gmail.com>
2017-02-06 21:51:08 +0000

    improved error messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
-  validates :comments, presence: true
+  validates :comments, presence: true, length: { in: 3..50 }
 end
